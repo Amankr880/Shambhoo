0787a09e4945ae441e27cc1f713b9d2fea2511e6
Amankr880 <amankr880@gmail.com>
2022-09-08 22:50:04 +0530

    changes

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Categories;
 use Storage;
+use Illuminate\Support\Facades\File;
 
 class CategoriesController extends Controller
 {
