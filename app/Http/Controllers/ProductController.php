091e4c5266bc7fd8c2dd0dd8cbf45684f7af0033
Mukul <mukul.m@hanu.com>
2022-11-30 19:39:30 +0530

    getvendorparent

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -177,11 +177,12 @@ class ProductController extends Controller
                 if($vendor_category[$i]['parent_category']==NULL || $vendor_category[$i]['parent_category']==0){
                     
                 }else{
-                    $vendor_parent=Category::where('id','=',$vendor_category[$i]['parent_category'])->select('categories.*'))->get();
+                    $vendor_parent=Category::where('id','=',$vendor_category[$i]['parent_category'])->select('categories.*')->get();
                     $vendor_category[$i]['category_name']=$vendor_parent[$i]['category_name'];
                     $vendor_category[$i]['description']=$vendor_parent[$i]['description'];
                     $vendor_category[$i]['icon']=$vendor_parent[$i]['icon'];
                 }
+            }
             $response = response()->json(['vendor_category'=>$vendor_category],200);
             
         }
