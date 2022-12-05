b4a1ec43a8e14dfa79f1f0a46c3a304dd1652048
Mukul <mukul.m@hanu.com>
2022-12-05 23:22:13 +0530

    subscriptionmodel

--- app/Http/Controllers/PlaceOrderController.php
+++ app/Http/Controllers/PlaceOrderController.php
@@ -35,9 +35,12 @@ class PlaceOrderController extends Controller
         $data->vendor_id = $request->vendor_id;
         $data->status = '0';
         $data->order_details = $request->description;
+        $data->delivery_address = $request->input('delivery_address');
         $data->order_date = $current_date;
         $data->order_otp = mt_rand(1000,9999);
         $data->order_type = $fileNameToStore;
+        $orderNo = 'SHMBO-'.mt_rand(100000,999999);
+        $data->order_no = $orderNo;
         $response = $data->save();
 
 
