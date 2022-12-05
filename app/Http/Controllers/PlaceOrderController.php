402922c93e8f573b9dfc6725401abedf05ed90ec
Mukul <mukul.m@hanu.com>
2022-12-05 23:14:33 +0530

    subscriptionmodel

--- app/Http/Controllers/PlaceOrderController.php
+++ app/Http/Controllers/PlaceOrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\Vendor;
 use Illuminate\Support\Carbon;
 
 class PlaceOrderController extends Controller
