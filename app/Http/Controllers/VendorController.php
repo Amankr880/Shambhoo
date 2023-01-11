5835a67d9f10f226aa0935145461ee729ddba4a3
Mukul <mukul.m@hanu.com>
2023-01-11 15:36:18 +0530

    productimgs

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -222,7 +222,7 @@ class VendorController extends Controller
         {
 
 
-        $vendor = Vendor::where('user_id','=',$request->id)->join('plan_subscriptions','vendors.id','=','plan_subscriptions.vendor_id')->select('vendors.*','plan_subscriptions.validity',DB::raw("CONCAT('storage/assets/img/logo/',logo_image) AS logo_image,CONCAT('storage/assets/img/header/',header_image) AS header_image"))->get();
+        $vendor = Vendor::where('user_id','=',$request->id)->leftJoin('plan_subscriptions','vendors.id','=','plan_subscriptions.vendor_id')->select('vendors.*','plan_subscriptions.validity',DB::raw("CONCAT('storage/assets/img/logo/',logo_image) AS logo_image,CONCAT('storage/assets/img/header/',header_image) AS header_image"))->get();
         //return $vendor;
 
         if(count($vendor)==0){
