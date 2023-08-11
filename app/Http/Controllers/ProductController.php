3c7aa142314f55bb3cc35bb94e92716fd62808a9
unknown <mukulyashi@gmail.com>
2023-08-11 23:47:58 +0530

    multipic

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,25 +43,25 @@ class ProductController extends Controller
             $destinationPath = "assets/img/product_img/";
             $pic = $file->hashName();
             Storage::disk('public')->putFileAs($destinationPath, $file, $pic);
-            $picture["picture1"] = $pic
+            $picture["picture1"] = $pic;
 
             $file = $request->file('picture2');
             $destinationPath = "assets/img/product_img/";
             $pic = $file->hashName();
             Storage::disk('public')->putFileAs($destinationPath, $file, $pic);
-            $picture["picture2"] = $pic
+            $picture["picture2"] = $pic;
 
             $file = $request->file('picture3');
             $destinationPath = "assets/img/product_img/";
             $pic = $file->hashName();
             Storage::disk('public')->putFileAs($destinationPath, $file, $pic);
-            $picture["picture3"] = $pic
+            $picture["picture3"] = $pic;
 
             $file = $request->file('picture4');
             $destinationPath = "assets/img/product_img/";
             $pic = $file->hashName();
             Storage::disk('public')->putFileAs($destinationPath, $file, $pic);
-            $picture["picture4"] = $pic
+            $picture["picture4"] = $pic;
 
             $product->picture = $picture;
 
